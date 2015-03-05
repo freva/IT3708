f337179acafe5ac5b9f519c9c5e4988cfb104ed9
Limon <valerijf@stud.ntnu.no>
2015-03-05 21:46:07 +0100

    Implemented adult selection

--- src/EA/generic/EA.java
+++ src/EA/generic/EA.java
@@ -2,11 +2,35 @@ package EA.generic;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class EA {
     private ArrayList<Genotype> population = new ArrayList<>();
+    private int populationSize = 0;
 
-    public void selectAdults(AdultSelection as) {
+
+    public void runGeneration() {
+
+    }
+
+
+    public void selectAdults(AdultSelection as, ArrayList<Genotype> children) {
+        switch (as) {
+            case FULL:
+                population = children;
+                break;
+
+            case OVER_PRODUCTION:
+                Collections.sort(children);
+                population = (ArrayList<Genotype>) children.subList(Math.max(children.size() - populationSize, 0), children.size());
+                break;
+
+            case MIXING:
+                children.addAll(population);
+                Collections.sort(children);
+                population = (ArrayList<Genotype>) children.subList(Math.max(children.size() - populationSize, 0), children.size());
+                break;
+        }
 
     }
 }
