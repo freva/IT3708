42014883b93b421778a88b4a3d91786691041373
Limon <valerijf@stud.ntnu.no>
2015-04-08 21:11:37 +0200

    Added graphics

--- src/ANN/project3/Flatland.java
+++ src/ANN/project3/Flatland.java
@@ -1,26 +1,56 @@
 package ANN.project3;
 
-import ANN.project3.cells.Cell;
+import ANN.project3.cells.EmptyCell;
 import ANN.project3.cells.Food;
+import ANN.project3.cells.Player;
 import ANN.project3.cells.Poison;
 
-public class Flatland {
-    Cell[][] board;
+import javax.swing.*;
+import java.awt.*;
+
+public class Flatland extends JPanel {
+    private EmptyCell[][] board;
+
+
+    public Flatland(EmptyCell[][] board) {
+        setBackground(Color.DARK_GRAY);
+        setLayout(null);
+        this.board = board;
+    }
 
     public Flatland(int dimension, double probPoison, double probFood) {
-        board = new Cell[dimension][dimension];
+        this(generateBoard(dimension, probPoison, probFood));
+    }
+
+
+    public static EmptyCell[][] generateBoard(int dimension, double probPoison, double probFood) {
+        EmptyCell[][] board = new EmptyCell[dimension][dimension];
+        System.out.println(probFood + " " + probPoison);
 
         for(int i=0; i<dimension; i++) {
             for(int j=0; j<dimension; j++) {
-                if(probFood < Math.random())
-                    board[i][j] = new Food(i, j);
-                else if(probPoison < Math.random())
-                    board[i][j] = new Poison(i, j);
+                if(Math.random() < probFood)
+                    board[i][j] = new Food();
+                else if(Math.random() < probPoison)
+                    board[i][j] = new Poison();
+                else
+                    board[i][j] = new EmptyCell();
             }
         }
+
+        board[(int) (Math.random()*dimension)][(int) (Math.random()*dimension)] = new Player();
+
+        return board;
     }
 
-    public Flatland(Cell[][] board) {
-        this.board = board;
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int i=0; i<board.length; i++) {
+            for (int j=0; j<board.length; j++) {
+                if (board[i][j] != null)
+                    board[i][j].draw(g, i*Project3.CELL_SIZE, j*Project3.CELL_SIZE);
+            }
+        }
     }
 }
--- src/ANN/project3/cells/Player.java
+++ src/ANN/project3/cells/Player.java
@@ -1,9 +1,29 @@
 package ANN.project3.cells;
 
-public class Player extends Cell {
-    private Direction direction = Direction.UP;
+import ANN.project3.Project3;
 
-    public Player(int x, int y) {
-        super(x, y);
+import java.awt.*;
+import java.awt.geom.GeneralPath;
+
+public class Player extends EmptyCell {
+    private Direction direction = Direction.UP;
+    private double[][] dynCoords = {{0.5, 0.85}, {0.85, 0.15}, {0.5, 0.3}, {0.15, 0.15}};
+
+    @Override
+    public void draw(Graphics g, int offsetX, int offsetY) {
+        super.draw(g, offsetX, offsetY);
+
+        Graphics2D g2 = (Graphics2D) g;
+        GeneralPath polygon = new GeneralPath(GeneralPath.WIND_EVEN_ODD, dynCoords.length);
+        polygon.moveTo(offsetX + dynCoords[0][0]* Project3.CELL_SIZE, offsetY + dynCoords[0][1]*Project3.CELL_SIZE);
+
+        for (int i = 1; i < dynCoords.length; i++)
+            polygon.lineTo(offsetX + dynCoords[i][0]*Project3.CELL_SIZE, offsetY + dynCoords[i][1]*Project3.CELL_SIZE);
+
+
+        polygon.closePath();
+        g2.setColor(Color.BLUE);
+        g2.fill(polygon);
+        g2.draw(polygon);
     }
 }
