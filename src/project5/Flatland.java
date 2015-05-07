a2a42b944b79bda0bdc0d2f7cba4192ca1ff3b05
Limon <valerijf@stud.ntnu.no>
2015-05-07 15:51:13 +0200

    Added runSimulation

--- src/project5/Flatland.java
+++ src/project5/Flatland.java
@@ -1,5 +1,8 @@
 package project5;
 
+import generics.QLearning.QLearner;
+import project5.cells.Direction;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -18,13 +21,34 @@ public class Flatland extends JPanel {
     }
 
 
+    public void runSimulation() {
+        QLearner qLearner = new QLearner(board, Direction.values().length-1, 0.8, 0.5);
+
+        for(int i=0; i<2000; i++) {
+            qLearner.runGeneration();
+            System.out.println("Iteration: " + (i+1) + " | " + qLearner.getLastGame());
+        }
+
+        simulateBestResult(qLearner);
+    }
+
+
+    private void simulateBestResult(QLearner qLearner) {
+        Project5.setUpGUI(this);
+        do {
+            board.updateGame(qLearner.selectAction(board.getHash()));
+            repaint();
+        } while(!board.isFinished());
+    }
+
+
     public void paint(Graphics g) {
         super.paint(g);
 
         if(board == null) return;
         g.setColor(Color.WHITE);
         g.setFont(new Font("Dialog", Font.PLAIN, 20));
-        g.drawString("Moves: " + board.getNumberOfMoves() + " | Food: " + board.getFoodEaten() + " | Poison: " + board.getPoisonEaten(), 10, 22);
+        g.drawString(board.toString(), 10, 22);
         for (int i=0; i<Board.BOARD_DIMENSION_X; i++) {
             for (int j=0; j<Board.BOARD_DIMENSION_Y; j++) {
                 board.getCell(i, j).draw(g, i*Project5.CELL_SIZE, INFO_BOARD_HEIGHT+j*Project5.CELL_SIZE);
